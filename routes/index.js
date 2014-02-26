6d64a04855e46d0e816c791c048cafff3cc6db03
Felipe Knorr Kuhn <git@knorrium.info>
2014-02-26 08:08:56 -0800

    basic listing of the public folder

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
-exports.index = function(req, res){
-	res.render('index', {
-      title: 'Castro Recordings'
+var fs = require('fs');
+
+exports.index = function(req, res) {
+    fs.readdir(__dirname + '/../public', function(error, data) {
+        console.log(data);
+        if (error) {
+            res.status(500).send(error);
+            return;
+        };
+        res.render('index', {
+            'title': 'Castro Recordings',
+            'files': data
+        });
     });
-}
+};
